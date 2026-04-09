f6485195107606ed302701f020722061c41904b2
neroliprak <neroliprak@gmail.com>
2026-04-09 15:52:02 +0200

    add bookDrawing class

--- src/books/BookInteraction.ts
+++ src/books/BookInteraction.ts
@@ -1,175 +1,181 @@
 import { Experience, type InputEventArgs } from "base-experience";
 import type InteractableObject from "../interactable/InteractableObject";
+import BookDrawing from "./BookDrawing";
 
 export default class BookInteraction {
-  declare bookSelectorInterface: HTMLElement;
-  declare bookDrawingInterface: HTMLElement;
-  declare bookInterface: HTMLElement;
-  declare closeBookSelectorButton: HTMLButtonElement;
-  declare closeBookDrawingButton: HTMLButtonElement;
-  declare isCloseToInteractable: InteractableObject | null;
-  declare isOpenBookSelector: boolean;
-  declare isOpenBookDrawing: boolean;
+	declare bookSelectorInterface: HTMLElement;
+	declare bookDrawingInterface: HTMLElement;
+	declare bookInterface: HTMLElement;
+	declare closeBookSelectorButton: HTMLButtonElement;
+	declare closeBookDrawingButton: HTMLButtonElement;
+	declare isCloseToInteractable: InteractableObject | null;
+	declare isOpenBookSelector: boolean;
+	declare isOpenBookDrawing: boolean;
 
-  declare isHalfOpenBookDrawing: boolean;
+	declare bookDrawing: BookDrawing | null;
 
-  //   @TODO : tableau d'objet récupéré depuis le Player
-  declare objectCollected: InteractableObject[] | null;
+	declare isHalfOpenBookDrawing: boolean;
 
-  constructor() {
-    const bookSelectorInterface = document.getElementById(
-      "book-selector-interface",
-    );
-    const bookDrawingInterface = document.getElementById(
-      "book-drawing-interface",
-    );
-    const bookInterface = document.getElementById("book-interface");
-    const closeBookSelectorButton = document.getElementById(
-      "close-book-selector",
-    );
-    const closeBookDrawingButton =
-      document.getElementById("close-book-drawing");
+	//   @TODO : tableau d'objet récupéré depuis le Player
+	declare objectCollected: InteractableObject[] | null;
 
-    if (
-      !bookSelectorInterface ||
-      !bookInterface ||
-      !closeBookSelectorButton ||
-      !bookDrawingInterface ||
-      !closeBookDrawingButton
-    ) {
-      return;
-    }
+	constructor() {
+		const bookSelectorInterface = document.getElementById(
+			"book-selector-interface",
+		);
+		const bookDrawingInterface = document.getElementById(
+			"book-drawing-interface",
+		);
+		const bookInterface = document.getElementById("book-interface");
+		const closeBookSelectorButton = document.getElementById(
+			"close-book-selector",
+		);
+		const closeBookDrawingButton =
+			document.getElementById("close-book-drawing");
 
-    this.bookSelectorInterface = bookSelectorInterface;
-    this.bookDrawingInterface = bookDrawingInterface;
-    this.bookInterface = bookInterface;
-    this.closeBookSelectorButton = closeBookSelectorButton as HTMLButtonElement;
-    this.closeBookDrawingButton = closeBookDrawingButton as HTMLButtonElement;
+		if (
+			!bookSelectorInterface ||
+			!bookInterface ||
+			!closeBookSelectorButton ||
+			!bookDrawingInterface ||
+			!closeBookDrawingButton
+		) {
+			return;
+		}
 
-    this.isHalfOpenBookDrawing = false;
+		this.bookSelectorInterface = bookSelectorInterface;
+		this.bookDrawingInterface = bookDrawingInterface;
+		this.bookInterface = bookInterface;
+		this.closeBookSelectorButton = closeBookSelectorButton as HTMLButtonElement;
+		this.closeBookDrawingButton = closeBookDrawingButton as HTMLButtonElement;
 
-    this.isOpenBookSelector = false;
-    this.isOpenBookDrawing = false;
-    this.isCloseToInteractable = null;
-    this.updateBookSelectorVisibility();
-    this.registerEventToggleBook();
-  }
+		this.isHalfOpenBookDrawing = false;
+		this.isOpenBookSelector = false;
+		this.isOpenBookDrawing = false;
 
-  //   Tous les événements d'affichage du carnet
-  registerEventToggleBook(): void {
-    if (!Experience.instance) return;
+		this.bookDrawing = new BookDrawing();
 
-    // INTERACTION 1 : Entrouvrir le carnet si le joueur proche obj interactif
-    Experience.instance.camera.on(
-      "onSelectedObjectChanged",
 
-      (args: InteractableObject | null) => {
-        this.isCloseToInteractable = args ?? null;
+		this.isCloseToInteractable = null;
+		this.updateBookSelectorVisibility();
+		this.registerEventToggleBook();
+	}
 
-        this.halfOpenBookDrawing();
+	//   Tous les événements d'affichage du carnet
+	registerEventToggleBook(): void {
+		if (!Experience.instance) return;
 
-        // Si le joueur s'éloigne de l'objet interactif où le carnet est déjà actif
-        if (!this.isCloseToInteractable) {
-          this.onCloseBookDrawing();
-        }
+		// INTERACTION 1 : Entrouvrir le carnet si le joueur proche obj interactif
+		Experience.instance.camera.on(
+			"onSelectedObjectChanged",
 
-        // Si le joueur a ouvert son carnet de sélection devant un objet interactif -> on ferme le carneet de sélection
-        if (this.isCloseToInteractable && this.isOpenBookSelector) {
-          this.setBookSelectorOpen(false);
-        }
-      },
-    );
+			(args: InteractableObject | null) => {
+				this.isCloseToInteractable = args ?? null;
 
-    // INTERACTION 3 : Ouverture du carnet de dessin en entier
-    Experience.instance?.camera.on(
-      "onInteractionPressed",
-      (args: InteractableObject | null) => {
-        console.log("arguments : ", args);
-        this.openBookDrawing();
-      },
-    );
+				this.halfOpenBookDrawing();
 
-    // INTERACTION 2 : Ouverture du carnet de sélection
-    Experience.instance.inputSystem.on("interact", this.onOpenBookSelector);
+				// Si le joueur s'éloigne de l'objet interactif où le carnet est déjà actif
+				if (!this.isCloseToInteractable) {
+					this.onCloseBookDrawing();
+				}
 
-    this.closeBookSelectorButton.addEventListener(
-      "click",
-      this.onCloseBookSelector,
-    );
-    this.closeBookDrawingButton.addEventListener(
-      "click",
-      this.onCloseBookDrawing,
-    );
-  }
+				// Si le joueur a ouvert son carnet de sélection devant un objet interactif -> on ferme le carneet de sélection
+				if (this.isCloseToInteractable && this.isOpenBookSelector) {
+					this.setBookSelectorOpen(false);
+				}
+			},
+		);
 
-  // Ouverture du carnet de drawing entièrement
-  openBookDrawing(): void {
-    if (!this.isCloseToInteractable) return;
-    console.log("ouverture du carnet en entier !");
-    this.bookSelectorInterface.style.display = "none";
-    this.bookInterface.style.display = "flex";
-    this.bookInterface.classList.remove("half-open-book-interface");
-    this.bookDrawingInterface.style.display = "flex";
-    this.isOpenBookDrawing = true;
-    document.exitPointerLock();
-    this.isHalfOpenBookDrawing = false;
-  }
+		// INTERACTION 3 : Ouverture du carnet de dessin en entier
+		Experience.instance?.camera.on(
+			"onInteractionPressed",
+			(args: InteractableObject | null) => {
+				console.log('arg name', args?.getId());
+				this.openBookDrawing();
+			},
+		);
 
-  halfOpenBookDrawing(): void {
-    if (!this.isCloseToInteractable) return;
-    console.log("entrouverture du carnet !");
-    this.bookSelectorInterface.style.display = "none";
-    this.bookInterface.style.display = "flex";
-    this.bookInterface.classList.add("half-open-book-interface");
-    this.bookDrawingInterface.style.display = "flex";
-    this.isOpenBookDrawing = true;
-    this.isHalfOpenBookDrawing = true;
-  }
+		// INTERACTION 2 : Ouverture du carnet de sélection
+		Experience.instance.inputSystem.on("interact", this.onOpenBookSelector);
 
-  onCloseBookDrawing = (): void => {
-    this.bookSelectorInterface.style.display = "none";
-    this.bookInterface.style.display = "none";
-    // this.bookInterface.classList.remove("half-open-book-interface");
-    this.bookDrawingInterface.style.display = "none";
-    this.isOpenBookDrawing = false;
-    document.body.requestPointerLock();
-    this.isHalfOpenBookDrawing = false;
-    true;
-  };
+		this.closeBookSelectorButton.addEventListener(
+			"click",
+			this.onCloseBookSelector,
+		);
+		this.closeBookDrawingButton.addEventListener(
+			"click",
+			this.onCloseBookDrawing,
+		);
+	}
 
-  /* INTERACTION POUR LA FERMETURE / OUVERTURE DU CARNET DE SELECTION */
+	// Ouverture du carnet de drawing entièrement
+	openBookDrawing(): void {
+		if (!this.isCloseToInteractable) return;
+		console.log("ouverture du carnet en entier !");
+		this.bookSelectorInterface.style.display = "none";
+		this.bookInterface.style.display = "flex";
+		this.bookInterface.classList.remove("half-open-book-interface");
+		this.bookDrawingInterface.style.display = "flex";
+		this.isOpenBookDrawing = true;
+		document.exitPointerLock();
+		this.isHalfOpenBookDrawing = false;
+	}
 
-  //  Clique sur "E" pour ouvrir le carnet de sélection
-  onOpenBookSelector = (args: InputEventArgs): void => {
-    if (args.type !== "pressed") {
-      return;
-    }
-    if (this.isCloseToInteractable === null) {
-      console.log("le joueur n'est pas proche de l'objet");
-      this.setBookSelectorOpen(!this.isOpenBookSelector);
-    }
-  };
+	halfOpenBookDrawing(): void {
+		if (!this.isCloseToInteractable) return;
+		console.log("entrouverture du carnet !");
+		this.bookSelectorInterface.style.display = "none";
+		this.bookInterface.style.display = "flex";
+		this.bookInterface.classList.add("half-open-book-interface");
+		this.bookDrawingInterface.style.display = "flex";
+		this.isOpenBookDrawing = true;
+		this.isHalfOpenBookDrawing = true;
 
-  //  Clique sur la croix pour fermer le carnet de sélection
-  onCloseBookSelector = (): void => {
-    this.setBookSelectorOpen(false);
-  };
+	}
 
-  //   Toggle la visibilité du carnet de sélection
-  setBookSelectorOpen = (isOpen: boolean): void => {
-    this.isOpenBookSelector = isOpen;
-    document.exitPointerLock();
-    this.updateBookSelectorVisibility();
-  };
+	onCloseBookDrawing = (): void => {
+		this.bookSelectorInterface.style.display = "none";
+		this.bookInterface.style.display = "none";
+		// this.bookInterface.classList.remove("half-open-book-interface");
+		this.bookDrawingInterface.style.display = "none";
+		this.isOpenBookDrawing = false;
+		document.body.requestPointerLock();
+		this.isHalfOpenBookDrawing = false;
+	};
 
-  //   Mettre à jour visibilité du style en HTML
-  updateBookSelectorVisibility = (): void => {
-    const displayBook = this.isOpenBookSelector ? "flex" : "none";
-    this.bookInterface.style.display = displayBook;
-    this.bookSelectorInterface.style.display = displayBook;
-    this.bookDrawingInterface.style.display = "none";
-    if (!this.isOpenBookSelector) {
-      document.body.requestPointerLock();
-    }
-  };
+	/* INTERACTION POUR LA FERMETURE / OUVERTURE DU CARNET DE SELECTION */
+
+	//  Clique sur "E" pour ouvrir le carnet de sélection
+	onOpenBookSelector = (args: InputEventArgs): void => {
+		if (args.type !== "pressed") {
+			return;
+		}
+		if (this.isCloseToInteractable === null) {
+			console.log("le joueur n'est pas proche de l'objet");
+			this.setBookSelectorOpen(!this.isOpenBookSelector);
+		}
+	};
+
+	//  Clique sur la croix pour fermer le carnet de sélection
+	onCloseBookSelector = (): void => {
+		this.setBookSelectorOpen(false);
+	};
+
+	//   Toggle la visibilité du carnet de sélection
+	setBookSelectorOpen = (isOpen: boolean): void => {
+		this.isOpenBookSelector = isOpen;
+		document.exitPointerLock();
+		this.updateBookSelectorVisibility();
+	};
+
+	//   Mettre à jour visibilité du style en HTML
+	updateBookSelectorVisibility = (): void => {
+		const displayBook = this.isOpenBookSelector ? "flex" : "none";
+		this.bookInterface.style.display = displayBook;
+		this.bookSelectorInterface.style.display = displayBook;
+		this.bookDrawingInterface.style.display = "none";
+		if (!this.isOpenBookSelector) {
+			document.body.requestPointerLock();
+		}
+	};
 }
