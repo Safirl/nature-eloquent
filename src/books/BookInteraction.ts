98af04bfea27aeea0c9fdda490a2ce101597bba4
neroliprak <neroliprak@gmail.com>
2026-04-09 12:30:02 +0200

    open book when player not close to an interact object

--- src/books/BookInteraction.ts
+++ src/books/BookInteraction.ts
@@ -3,20 +3,37 @@ import type InteractableObject from "../interactable/InteractableObject";
 
 export default class BookInteraction {
   declare bookSelectorInterface: HTMLElement;
+  declare bookInterface: HTMLElement;
+  declare closeBookSelectorButton: HTMLButtonElement;
   declare isCloseToInteractable: InteractableObject | null;
-  declare isOpen: boolean;
+  declare isOpenBookSelector: boolean;
+
   constructor() {
-    // Récupération du DOM book-slector-interface
     const bookSelectorInterface = document.getElementById(
       "book-selector-interface",
     );
+    const bookInterface = document.getElementById("book-interface");
+    const closeBookSelectorButton = document.getElementById(
+      "close-book-selector",
+    );
+
     if (!bookSelectorInterface) {
       return;
     }
-    this.bookSelectorInterface = bookSelectorInterface;
+    if (!bookInterface) {
+      return;
+    }
+    if (!closeBookSelectorButton) {
+      return;
+    }
 
-    this.isOpen = false;
+    this.bookSelectorInterface = bookSelectorInterface;
+    this.bookInterface = bookInterface;
+    this.closeBookSelectorButton = closeBookSelectorButton as HTMLButtonElement;
+
+    this.isOpenBookSelector = false;
     this.isCloseToInteractable = null;
+    this.updateBookSelectorVisibility();
     this.registerEventToggleBook();
   }
 
@@ -30,22 +47,46 @@ export default class BookInteraction {
       (args: InteractableObject | null) => {
         this.isCloseToInteractable = args ?? null;
         console.log(
-          "is close to an interactable object",
+          "Entrouvrir le carnet de dessin avec l'objet :",
           this.isCloseToInteractable,
         );
       },
     );
 
     // INTERACTION 2 : Ouverture du carnet de sélection
-    Experience.instance.inputSystem.on("interact", this.onInteract);
+    Experience.instance.inputSystem.on("interact", this.onOpenBookSelector);
+    this.closeBookSelectorButton.addEventListener(
+      "click",
+      this.onCloseBookSelector,
+    );
   }
 
-  onInteract = (args: InputEventArgs): void => {
+  //   INTERACTION 2 : Ouverture du carnet de sélection
+  onOpenBookSelector = (args: InputEventArgs): void => {
     if (args.type !== "pressed") {
       return;
     }
     if (this.isCloseToInteractable === null) {
       console.log("le joueur n'est pas proche de l'objet");
+      this.setBookSelectorOpen(!this.isOpenBookSelector);
     }
+  };
+
+  //   INTERACTION 2 : Fermeture du carnet de sélection
+  onCloseBookSelector = (): void => {
+    this.setBookSelectorOpen(false);
+  };
+
+  //   INTERACTION 2 : On affiche ou non le carnet de sélection
+  setBookSelectorOpen = (isOpen: boolean): void => {
+    this.isOpenBookSelector = isOpen;
+    this.updateBookSelectorVisibility();
+  };
+
+  //   INTERACTION 2 : Permet de togler la visibilité du carnet de sélection
+  updateBookSelectorVisibility = (): void => {
+    const displayBook = this.isOpenBookSelector ? "flex" : "none";
+    this.bookInterface.style.display = displayBook;
+    this.bookSelectorInterface.style.display = displayBook;
   };
 }
