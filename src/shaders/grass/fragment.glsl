1121ca26d39556e3a60e708f699713fb2a20548a
Emilien <emilien.ginko@gmail.com>
2026-04-28 18:06:40 +0200

    feat(Grass): make grass support a darkDactor to make it support the selective bloom

--- src/shaders/grass/fragment.glsl
+++ src/shaders/grass/fragment.glsl
@@ -1,12 +1,12 @@
 varying vec2 vUv;
 uniform sampler2D uGrassMapTexture;
 uniform sampler2D uGrassAlphaMap;
-
+uniform vec3 uDarkFactor;
 void main() {
     vec3 finalColor = texture2D(uGrassMapTexture, vUv).rgb;
     float alpha = texture2D(uGrassAlphaMap, vUv).r;
-    if (alpha < .9) {
+    if(alpha < .9) {
         discard;
     }
-    gl_FragColor = vec4(finalColor, alpha);
+    gl_FragColor = vec4(finalColor * uDarkFactor, alpha);
 }
