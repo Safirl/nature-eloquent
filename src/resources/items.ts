20693eb94ac673fb54fceced7c3e020eec3d276d
Emilien <emilien.ginko@gmail.com>
2026-05-06 16:15:51 +0200

    chore(items): change stickers images

--- src/resources/items.ts
+++ src/resources/items.ts
@@ -1,96 +1,131 @@
-import mushroomIcon from "../books/mushroom.png";
-import mushroom2Icon from "../books/mushroom2.png";
-
 const itemsList: MenuItemType[] = [
+
+	/**
+	 * Exists 
+	 * */
+	{ 
+		id: "edeilweiss",
+		name: "Edeilweiss",
+		model: "mushroomPaintedModel",
+		vignet: "edeilweiss_flower",
+		shadow: "edeilweiss_flower_shadow"
+	},
+	{
+		id: "buttercup",
+		name: "Boutton d'or",
+		model: "mushroomPaintedModel",
+		vignet: "buttercup_flower",
+		shadow: "buttercup_flower_shadow"
+	},
+	{
+		id: "giant_iris",
+		name: "Iris Géante",
+		model: "mushroomPaintedModel",
+		vignet: "iris_stem_flower",
+		shadow: "iris_stem_flower_shadow"
+	},
+	{
+		id: "giant_buttercup",
+		name: "Boutton d'or Géant",
+		model: "mushroomPaintedModel",
+		vignet: "buttercup_stem_flower",
+		shadow: "buttercup_stem_flower_shadow"
+	},
+	{
+		id: "iris",
+		name: "Iris",
+		model: "mushroomPaintedModel",
+		vignet: "iris_flower",
+		shadow: "iris_flower_shadow"
+	},
+	{
+		id: "lys",
+		name: "Lys",
+		model: "mushroomPaintedModel",
+		vignet: "lys_flower",
+		shadow: "lys_flower_shadow"
+	},
+
+
+
+
+
+
+
+
+	/**
+	 * Doesn't exist
+	 */
 	{
 		id: "dinosaur",
 		name: "Dinosaure",
 		model: "mushroomPaintedModel",
 		vignet: "mushroom",
+		shadow: "mushroom",
 	},
 	{
 		id: "postcard",
 		name: "Carte postale",
 		model: "mushroomModel",
-		vignet: "mushroom2",
+		vignet: "mushroom",
+		shadow: "mushroom"
 	},
 	{
 		id: "vine",
 		name: "Liane",
 		model: "mushroomModel",
-		vignet: mushroom2Icon,
+		vignet: "mushroom",
+		shadow: "mushroom"
 	},
 	{
 		id: "grassClump",
 		name: "Touffe d'herbe",
 		model: "mushroomModel",
-		vignet: mushroom2Icon,
+		vignet: "mushroom",
+		shadow: "mushroom"
 	},
 	{
 		id: "fern",
 		name: "Fougère",
 		model: "mushroomModel",
-		vignet: mushroom2Icon,
-	},
-	{
-		id: "ppFlower",
-		name: "mushroomCouc",
-		model: "mushroomPaintedModel",
 		vignet: "mushroom",
+		shadow: "mushroom"
 	},
-	{
-		id: "ppFlower1",
-		name: "mushroomCouc",
-		model: "mushroomPaintedModel",
-		vignet: mushroomIcon,
-	},
-	{
-		id: "giantPPFlower",
-		name: "pp grande fleur",
-		model: "mushroomPaintedModel",
-		vignet: mushroomIcon,
-	},
-	{
-		id: "clemFlower",
-		name: "clem fleur",
-		model: "mushroomPaintedModel",
-		vignet: mushroomIcon,
-	},
-	{
-		id: "clemFlower1",
-		name: "clem fleur",
-		model: "mushroomPaintedModel",
-		vignet: mushroomIcon,
-	},
+
 	{
 		id: "giantClemFlower",
 		name: "Clem grande fleur",
 		model: "mushroomPaintedModel",
-		vignet: mushroomIcon,
+		vignet: "mushroom",
+		shadow: "mushroom"
 	},
 	{
 		id: "butterfly",
 		name: "Papillon",
 		model: "mushroomPaintedModel",
-		vignet: mushroomIcon,
+		vignet: "mushroom",
+		shadow: "mushroom"
 	},
 	{
 		id: "bramble",
 		name: "Ronces",
 		model: "mushroomPaintedModel",
-		vignet: mushroomIcon,
+		vignet: "mushroom",
+		shadow: "mushroom"
 	},
 	{
 		id: "toxicMushroom",
 		name: "Champignon vénéneux",
 		model: "mushroomPaintedModel",
-		vignet: mushroomIcon,
+		vignet: "mushroom",
+		shadow: "mushroom"
 	},
 	{
 		id: "deadwood",
 		name: "Bois mort",
 		model: "mushroomPaintedModel",
-		vignet: mushroomIcon,
+		vignet: "mushroom",
+		shadow: "mushroom"
 	},
 ];
 
@@ -100,4 +135,5 @@ export type MenuItemType = {
 	name: string;
 	model: string;
 	vignet: string;
+	shadow: string;
 };
