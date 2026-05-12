1e4c762d94624f89d0fd1593104c23aa02d4bd7e
Emilien <emilien.ginko@gmail.com>
2026-05-12 12:21:06 +0200

    feat(room): add room to the scene

--- src/world/Room.ts
+++ src/world/Room.ts
@@ -6,9 +6,29 @@ import LavaLamp from "./LavaLamp";
 
 export default class Room {
     declare lavalamp: LavaLamp
+    declare private experience: Experience;
+    declare private debugFolder: GUI;
+    private declare scene: THREE.Scene;
+    private position = new THREE.Vector3(0, 0.3, 0)
+
 
     constructor() {
+        if (!Experience.instance) return;
+
+        this.experience = Experience.instance;
+        this.scene = this.experience.scene;
         this.lavalamp = new LavaLamp()
+
+
+
+        const gltf = this.experience.resources.items.room as GLTF;
+        const walls = gltf.scene.children.find((o: any) => o.name === "walls")
+        if (walls) {
+
+            walls.material.side = THREE.DoubleSide
+        }
+        gltf.scene.position.copy(this.position);
+        this.scene.add(gltf.scene);
     }
 
     init() {
